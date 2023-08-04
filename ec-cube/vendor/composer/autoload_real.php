82eafb89ce0550de9778b4f136816347224a4d48
r.sakamoto <r.sakamoto@TM-N812noMacBook-Pro.local>
2023-08-04 12:17:31 +0900

    composer self-update --2 訂正

--- ec-cube/vendor/composer/autoload_real.php
+++ ec-cube/vendor/composer/autoload_real.php
@@ -33,25 +33,18 @@ class ComposerAutoloaderInit2c0f8ba1f96add52ba0ba036f5e50253
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit2c0f8ba1f96add52ba0ba036f5e50253::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire2c0f8ba1f96add52ba0ba036f5e50253($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit2c0f8ba1f96add52ba0ba036f5e50253::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire2c0f8ba1f96add52ba0ba036f5e50253($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
